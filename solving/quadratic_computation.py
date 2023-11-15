f5383a862eb9667bd1cdbc4cc86c1ad67a55d358
Simon Samuel <salviensky@gmail.com>
2023-11-15 19:39:51 +0100

    feat: added new question

---
+++ solving/quadratic_computation.py
@@ -0,0 +1,17 @@
+# Write a function that returns the root of a quadratic equation,
+# It should take 3 parameters and return a tuple of at most 2 numbers.
+
+
+def quadratic_calculator(a: float, b: float, c: float):
+    x = 0
+    y = 0
+
+    # Your code goes here
+
+    return (x, y)
+
+
+if __name__ == "__main__":
+    x, y = quadratic_calculator(1, 5, 6)
+
+    print(f"The roots of the equation are {x} and {y}")
