313809d7a50695d3f6ee3e99cf6ed452b5b8f9d8
Simon Samuel <salviensky@gmail.com>
2023-11-15 19:32:06 +0100

    feat: added other implementations of solution

--- solved/sum_matrix.py
+++ solved/sum_matrix.py
@@ -7,11 +7,35 @@ data: list[list[int]] = [
     [15, 23, 24],
 ]
 
-sum = 0
 
-# Your code goes here.
+index_sum = 0
+value_sum = 0
+
+# Access by index
 for i in range(0, len(data)):
     for j in range(0, len(data[i])):
-        sum += data[i][j]
+        index_sum += data[i][j]
 
-print(f"The value of sum is {sum}")
+print(f"The value of sum using index is {index_sum}")
+
+
+# Access by value
+for item in data:
+    for n in item:
+        value_sum += n
+
+print(f"The value of sum using value is {value_sum}")
+
+
+# Access by enumerate
+for i, v in enumerate(data):
+    print(f"Index is {i} and value is {v}")
+
+# This is what enumerate function will return based on the argument we passed
+# into it. It returns a lists of index and value pairs. This is the return list
+# under the hood.
+# [
+#     [0, [13, 15, 16]],
+#     [1, [12, 29, 50]],
+#     [2, [15, 23, 24]],
+# ]
